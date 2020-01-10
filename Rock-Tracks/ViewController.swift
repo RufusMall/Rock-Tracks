c7cff2610659ab0848d049dba428b62b540dc8c1
Rufus <Rufusmall@hotmail.com>
2020-01-10 20:43:01 +0000

    working single view application

---
+++ Rock-Tracks/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Rock-Tracks
+//
+//  Created by Rufus on 10/01/2020.
+//  Copyright © 2020 Rufus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
