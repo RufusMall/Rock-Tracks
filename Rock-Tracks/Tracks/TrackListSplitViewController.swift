3019324fcbe884ca0ad9a96f0745125505f21b90
Rufus <Rufusmall@hotmail.com>
2020-01-10 21:34:47 +0000

    merge initialSetup: Initial project/CI setup. Has basic navigation in place, and some placeholder tests that CI builds.
    Squashed commit of the following:
    
    commit 388b7c8bab8379179d7ac9836bff29993542cbfe
    Author: Rufus <Rufusmall@hotmail.com>
    Date:   Fri Jan 10 21:33:43 2020 +0000
    
        Move views into folders
    
    commit 0facae9b8b816606a787c4c8ecff8089c7dc525e
    Author: RufusMall <Rufusmall@hotmail.com>
    Date:   Fri Jan 10 21:30:17 2020 +0000
    
        Readme: Link to CI Build status (this also runs unit tests) and a link to trello board
    
    commit b424d6acea5a1dcbe8a6dd85551313321d5d4dc7
    Author: Rufus <Rufusmall@hotmail.com>
    Date:   Fri Jan 10 21:25:53 2020 +0000
    
        Initial navigation configuration built. Uses UISplitviewcontroller for naivgation, tableview (for now) to show tracks and a details page has been added. Also tested on iPad
    
    commit 5a24a09d43b306dc2f76d966d13eeb9c1d36f806
    Author: Rufus <Rufusmall@hotmail.com>
    Date:   Fri Jan 10 20:47:38 2020 +0000
    
        remove scene delegate stuff, this does not compile on iOS 9.3

--- Rock-Tracks/Tracks/TrackListSplitViewController.swift
+++ Rock-Tracks/Tracks/TrackListSplitViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  TrackListSplitViewController.swift
 //  Rock-Tracks
 //
 //  Created by Rufus on 10/01/2020.
@@ -8,13 +8,9 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class TrackListSplitViewController: UISplitViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.preferredDisplayMode = .allVisible
     }
-
-
 }
-
